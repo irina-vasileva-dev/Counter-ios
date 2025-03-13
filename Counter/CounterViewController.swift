b37ecf66d3f65d736d4bab55942c3b926dec70ef
Irina Vasileva <alex.grin15@yandex.ru>
2025-03-13 17:19:48 +0300

    Добавлена реализация счетчика(усложненный вариант)

---
+++ Counter/CounterViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Irina Vasileva on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .portrait }
+    
+    @IBOutlet weak var countNumberLabel: UILabel!
+    
+    @IBOutlet weak var changeHistoryTextView:UITextView!
+    
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    @IBOutlet weak var decrementButton: UIButton!
+    
+    //переменная для хранения значения счетчика
+    private var counter = 0 {
+        didSet {
+            countNumberLabel.text = "\(counter)"
+        }
+    }
+ 
+    private let historyKey = "counterHistory"
+    
+    private var history: [String] = [] {
+        didSet {
+            UserDefaults.standard.set(history, forKey: historyKey)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        incrementButton.setTitleColor(.white, for: .normal)
+        incrementButton.backgroundColor = .red
+        decrementButton.setTitleColor(.white, for: .normal)
+        decrementButton.backgroundColor = .systemBlue
+        countNumberLabel.text = "0"
+        loadHistory()
+        updateHistoryTextView()
+        changeHistoryTextView.isEditable = false
+    }
+    
+    /*
+     * действие при нажатии на кнопку "+"
+     */
+    @IBAction func
+    incrementBtnTapped(_ sender: UIButton, forEvent event: UIEvent) {
+        counter += 1
+        let message = "[\(getCurrentDateTime())]: значение изменено на +1"
+        updateHistory(with: message)
+    }
+    
+    /*
+     * действие при нажатии на кнопку "-"
+     */
+    @IBAction func
+    decrementBtnTapped(_ sender: UIButton) {
+        if counter > 0 {
+            counter -= 1
+            
+            let message = "[\(getCurrentDateTime())]: значение изменено на -1"
+            updateHistory(with: message)
+        } else {
+            let message = "[\(getCurrentDateTime())]: попытка уменьшить значение счетчика ниже 0"
+            updateHistory(with: message)
+        }
+    }
+    
+    /*
+     * действие при нажатии на кнопку "сбросить"
+     */
+    @IBAction func
+    refreshBtnTapped(_ sender: UIButton) {
+        counter = 0
+        let message = "[\(getCurrentDateTime())]: значение сброшено"
+        updateHistory(with: message)
+    }
+
+    private func updateHistoryTextView() {
+        changeHistoryTextView.text = history.joined(separator: "\n")
+    }
+    /*
+     * функция для обновления истории изменений
+     */
+    private func updateHistory(with message: String) {
+        history.append(message)
+        updateHistoryTextView()
+        
+        let range = NSRange(location: changeHistoryTextView.text.count - 1, length: 1)
+        
+        changeHistoryTextView.scrollRangeToVisible(range)
+    }
+    
+    private func loadHistory() {
+        if let savedHistory = UserDefaults.standard.array(forKey: historyKey) as? [String] {
+            history = savedHistory
+        }
+        else {
+            history = ["История изменений: "]
+        }
+    }
+    
+    /*
+     * функция для получения текущей даты и времени и форматирования в нужный вид
+     */
+    private func getCurrentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
+}
+
